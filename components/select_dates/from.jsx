0b0e5e3e8719ad7672074080e5b90d5967b1396c
Oscar Gomez <oscarthedev@gmail.com>
2022-03-18 20:00:09 -0500

    [ADD] select_dates components created and styled

---
+++ components/select_dates/from.jsx
@@ -0,0 +1,19 @@
+export default function From() {
+	return (
+		<div className="select_date from">
+			<div className="select_date-title_container">
+				<div className="select_date-title_container-text">
+					From
+				</div>
+			</div>
+			<div className="select_date-date">
+				<div className="select_date-date-text">
+					2022-04-08
+				</div>
+			</div>
+			<div className="select_date-input_container">
+				<input className="select_date-input_container-input" type="date" />
+			</div>
+		</div>
+	)
+}
---
+++ components/select_dates/to.jsx
@@ -0,0 +1,19 @@
+export default function To() {
+	return (
+		<div className="select_date to">
+			<div className="select_date-title_container">
+				<div className="select_date-title_container-text">
+					To
+				</div>
+			</div>
+			<div className="select_date-date">
+				<div className="select_date-date-text">
+					2022-04-08
+				</div>
+			</div>
+			<div className="select_date-input_container">
+				<input className="select_date-input_container-input" type="date" />
+			</div>
+		</div>
+	)
+}
