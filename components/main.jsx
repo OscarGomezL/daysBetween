ce921259ed63c564f16dd10e2a361726386019f8
Oscar Gomez <oscarthedev@gmail.com>
2022-03-18 20:01:35 -0500

    [ADD] components imported to main

--- components/main.jsx
+++ components/main.jsx
@@ -1,5 +1,16 @@
+import { useState } from "react"
+import From from '../components/select_dates/from'
+import To from '../components/select_dates/to'
+import Calculate from "./calculate"
+
 export default function Main() {
+	const [from,setFrom] = useState()
+	const [to,setTo] = useState()
 	return (
-		<main>Main</main>
+		<main>
+			<From/>
+			<Calculate/>
+			<To/>
+		</main>
 	)
 }
