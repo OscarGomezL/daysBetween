ea4eec92fa0b81efed2582ae8b91e77256d05f4d
Oscar Gomez <oscarthedev@gmail.com>
2022-03-19 15:23:54 -0500

    [ADD] title to the page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,15 @@
 import Header from "../components/header"
 import Footer from "../components/footer"
 import Main from "../components/main"
+import Head from "next/head"
 
 export default function Home() {
 	return (
 		<div>
+			<Head>
+				<title>daysBetween</title>
+			</Head>
+
 			<Header/>
 			<Main/>
 			<Footer/>
