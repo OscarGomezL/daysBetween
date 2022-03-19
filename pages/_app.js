1875f185ba4b4f46806bfe4279b6582948234fd9
Oscar Gomez <oscarthedev@gmail.com>
2022-03-19 15:24:16 -0500

    [ADD] css imports to _app file

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,13 @@
+import '../styles/variables.css'
+import '../styles/text.css'
+import '../styles/global.css'
+import '../styles/reset.css'
 import '../styles/footer.css'
 import '../styles/main.css'
 import '../styles/header.css'
+import '../styles/calculate.css'
+import '../styles/select_dates.css'
+import '../styles/date_input.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
