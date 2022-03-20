fcc3620831de4ac4bef686707d8d638d91033921
Oscar Gomez <oscarthedev@gmail.com>
2022-03-20 01:41:32 -0500

    [ADD] 1 new state & imported Result

--- components/main.jsx
+++ components/main.jsx
@@ -2,15 +2,18 @@ import { useState } from "react"
 import From from '../components/select_dates/from'
 import To from '../components/select_dates/to'
 import Calculate from "./calculate"
+import Result from "./result"
 
 export default function Main() {
 	const [from,setFrom] = useState()
 	const [to,setTo] = useState()
+	const [result, setResult] = useState('N')
 	return (
 		<main>
-			<From/>
-			<Calculate/>
-			<To/>
+			<From from={from} setFrom={setFrom} />
+			<Calculate from={from} to={to} setResult={setResult}/>
+			<To to={to} setTo={setTo}/>
+			<Result result={result}/>
 		</main>
 	)
 }
