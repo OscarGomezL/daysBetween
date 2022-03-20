8fd7edc9297d7fe2e731a003d2d4b50dbbf47993
Oscar Gomez <oscarthedev@gmail.com>
2022-03-20 01:42:25 -0500

    [ADD] now imports the function and calculates given the values

--- components/calculate.jsx
+++ components/calculate.jsx
@@ -1,7 +1,12 @@
-export default function Calculate() {
+import { returnDiff } from "../logic/functions"
+
+export default function Calculate({to, from, setResult}) {
 	return (
-		<div className="calculate">
-			Calculate
+		<div 
+			className="calculate"
+			onClick={()=>setResult(returnDiff(new Date(from), new Date(to)))}
+		>
+			<div className="calculate-text">Calculate</div>
 		</div>
 	)
 }
