76d3d61097879cc827ff964c00e285a5139b8b96
Oscar Gomez <oscarthedev@gmail.com>
2022-04-16 12:35:11 -0600

    [ADD] "returnTImeMeasurement" implemented + new state added

--- components/main.jsx
+++ components/main.jsx
@@ -1,19 +1,24 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import From from '../components/select_dates/from'
 import To from '../components/select_dates/to'
-import Calculate from "./calculate"
 import Result from "./result"
+import {returnDiff, returnTimeMeasurement} from '../logic/functions'
 
 export default function Main() {
-	const [from,setFrom] = useState()
-	const [to,setTo] = useState()
+	const [measurement, setMeasurement] = useState("seconds")
+	const [from,setFrom] = useState('')
+	const [to,setTo] = useState('')
 	const [result, setResult] = useState('N')
+	useEffect(() => {
+		setResult(returnDiff(new Date(from), new Date(to), returnTimeMeasurement(measurement)))
+	}, [from, to, measurement])
+	
+	//<Calculate from={from} to={to} setResult={setResult}/>
 	return (
 		<main>
 			<From from={from} setFrom={setFrom} />
-			<Calculate from={from} to={to} setResult={setResult}/>
 			<To to={to} setTo={setTo}/>
-			<Result result={result}/>
+			<Result result={result} measurement={measurement} setMeasurement={setMeasurement}/>
 		</main>
 	)
 }
--- components/result.jsx
+++ components/result.jsx
@@ -1,7 +1,25 @@
-export default function Result({result}) {
+export default function Result({result, measurement, setMeasurement}) {
 	return (
-		<div className="result">
-			<div className="result-text">{isNaN(result)? 'Select a Date':(result+" days")}</div>
+		<div className="result"> 
+			<div className="result-text">
+				<div>
+					{isNaN(result)? 'Select a Date':(result+" " + measurement)}
+				</div>
+			</div>
+			<div className="result-controls">
+				<select
+					className="result-select"
+					onChange={(e)=>setMeasurement(e.target.value)}
+				>
+					<option value="seconds">Seconds</option>
+					<option value="minutes">Minutes</option>
+					<option value="hours">Hours</option>
+					<option value="days">Days</option>
+				</select>
+				<div>
+
+				</div>
+			</div>
 		</div>
 	)
 }
