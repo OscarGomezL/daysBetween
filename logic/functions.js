0000f0ec8727273329d0f5c1e4c239762248bd08
Oscar Gomez <oscarthedev@gmail.com>
2022-04-16 12:34:22 -0600

    [ADD] new "returnTImeMeasurement" function

--- logic/functions.js
+++ logic/functions.js
@@ -1,3 +1,15 @@
-export function returnDiff (from,to) {
-	return Math.ceil(Math.abs(from.getTime() - to.getTime()) / 86400000)
+export function returnDiff (from,to, measurement) {
+	return Math.ceil(Math.abs(from.getTime() - to.getTime()) / measurement)
+}
+export function returnTimeMeasurement (key) {
+	switch (key) {
+		case "seconds":
+			return 1000;
+		case "minutes" :
+			return 60000;
+		case "hours" :
+			return 3600000;
+		case "days" :
+			return 86400000;
+	}
 }
